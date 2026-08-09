3b8bc20ae290f5c6d87cc91517bddd87d404ad04
Eren Yahya Yöndeş <eyahyayondes12@gmail.com>
2026-08-09 05:34:01 +0300

    feat : implement sentinel node in linked lists

--- 2-doubly-linked-lists/sentinelLists.c
+++ 2-doubly-linked-lists/sentinelLists.c
@@ -1,2 +1,45 @@
 #include "sentinelLists.h"
 #include <stdlib.h>
+
+void initList(sentinelList *list)
+{
+    list->header = (intNode *)malloc(sizeof(intNode));
+    list->tailer = (intNode *)malloc(sizeof(intNode));
+
+    list->header->next = list->tailer;
+    list->header->previous = NULL;
+    list->tailer->previous = list->header;
+    list->tailer->next = NULL;
+}
+void addBetween(intNode *newNode, intNode *pred, intNode *succ)
+{
+    newNode->previous = pred;
+    newNode->next = succ;
+    pred->next = newNode;
+    succ->previous = newNode;
+}
+
+void insertFrontSentinel(sentinelList *list, intNode *newNode)
+{
+    addBetween(newNode, list->header, list->header->next);
+}
+
+void insertEndSentinel(sentinelList *list, intNode *newNode)
+{
+    addBetween(newNode, list->tailer->previous, list->tailer);
+}
+
+void removeSentinel(intNode *target)
+{
+    intNode *pred = target->previous;
+    intNode *succ = target->next;
+    pred->next = succ;
+    succ->previous = pred;
+
+    free(target);
+}
+
+int isEmptySentinel(sentinelList *list)
+{
+    return list->header->next == list->tailer;
+}
